6ca380bfec761f01a6ebf47431958101ff0f80b5
Tony Mashkoff <tony.mashkoff@gmail.com>
2016-03-13 22:35:26 +0300

    0.2 Added correct endings feature to script

--- goto_messages_list.user.js
+++ goto_messages_list.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Direct Link To Messages List
-// @version     0.1
+// @version     0.2
 // @namespace   EjudgeFE
-// @description Скрипт делает текст «n непрочитанных сообщений» ссылкой на страницу с сообщениями.
+// @description Скрипт делает текст «n непрочитанных сообщений» ссылкой на страницу с сообщениями и устанавливает правильное склонение.
 // @include     https://unicorn.ejudge.ru/ej/client/*
 // @include     https://cmc.ejudge.ru/ej/client/*
 // @author      Mashkoff Tony
@@ -10,6 +10,34 @@
 // ==/UserScript==
 
 (function() {
+    var locales = {
+        en : [ ' unread message', ' unread messages', ' unread messages' ],
+        ru : [ ' непрочитанное сообщение', ' непрочитанных сообщения', ' непрочитанных сообщений' ]
+    };
+
+    var locale;
+
+    if (document.getElementsByClassName('contest_actions_item')[0].innerHTML.indexOf('Настройки') > -1) {
+        locale = locales.ru;
+    } else {
+        locale = locales.en;
+    }
+
+    function getStringWithCorrectEndings(n, locale) {
+        n = n % 100;
+        if (11 <= n && n <= 19) {
+            return n + locale[2];
+        }
+        n = n % 10;
+        if (n == 1) {
+            return n + locale[0];
+        }
+        if (2 <= n && n <= 4) {
+            return n + locale[1];
+        }
+        return n + locale[2];
+    }
+
     var a = document.querySelectorAll('a');
 
     var clarsHref;
@@ -26,7 +54,8 @@
         var text = b[j].innerHTML;
         if (/\d+ непрочитанных сообщений/.test(text) ||
             /\d+ unread message\(s\)/.test(text)) {
-            b[j].innerHTML = '<a href=' + clarsHref + '>' + text + '</a>';
+            var num = parseInt(/\d+/.exec(text)[0]);
+            b[j].innerHTML = '<a href=' + clarsHref + '>' + getStringWithCorrectEndings(num, locale) + '</a>';
             break;
         }
     }
